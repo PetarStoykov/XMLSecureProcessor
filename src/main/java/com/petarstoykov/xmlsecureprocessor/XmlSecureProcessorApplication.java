07075aafc00648ec72c1e8aabd6fe12ff17f4967
Petar Stoykov <p.stoykov122@gmail.com>
2024-11-30 13:50:44 +0200

    Add SpringBoot project

---
+++ src/main/java/com/petarstoykov/xmlsecureprocessor/XmlSecureProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.petarstoykov.xmlsecureprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlSecureProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlSecureProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petarstoykov/xmlsecureprocessor/XmlSecureProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petarstoykov.xmlsecureprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlSecureProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
